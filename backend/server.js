99f37acc73e4d695a404a1349a96d24c29335f8c
Sounak Moitra <sounakmoitra04@gmail.com>
2026-07-29 00:06:44 +0530

    user schema creation

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));  --> this causes issues
 
 app.use(express.json({ limit: "1mb" })); // json parsing with size limit
 // for preventing large payloads from affecting performance
